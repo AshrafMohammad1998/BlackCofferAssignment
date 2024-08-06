2381076e0e9019c5eeae776a88705951ff8c877f
unknown <mohmmadashrafzphs@gmail.com>
2024-08-06 23:14:49 +0530

    added build folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from "axios"
 import Header from "./components/header/Header"
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import configVariables from './configurations/Config'
@@ -27,6 +28,23 @@ function App() {
   return (
     <div className="pt-6 bg-white dark:bg-slate-800 dark:text-white flex flex-col min-h-screen">
       <Header />
+      <div className="chart-container">
+        <BarChart
+          width={600}
+          height={300}
+          data={dashboardData}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="topic" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="intensity" fill="#8884d8" />
+          <Bar dataKey="likelihood" fill="#82ca9d" />
+          <Bar dataKey="relevance" fill="#ffc658" />
+        </BarChart>
+      </div>
       <main>
         <Outlet />
       </main>
